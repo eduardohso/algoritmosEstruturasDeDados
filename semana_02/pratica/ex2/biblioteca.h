57dbc84e898180b8379ac6f00eac9a6bbd3ccbf7
Eduardo Oliveira <eduardohsoliveira1@gmail.com>
2021-12-07 22:35:54 -0300

    resolvido

---
+++ semana_02/pratica/ex2/biblioteca.h
@@ -0,0 +1,40 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+#define TAML 100
+
+typedef struct sContato
+{
+    char nomeContato[21];
+    char numCelular[15];
+}Contato;
+
+
+typedef struct sLista
+{
+    Contato vetor[TAML];
+    int n;
+}Lista;
+
+void lista_inicializar(Lista *ptrL){
+    ptrL -> n = -1;
+}
+
+int lista_cheia(Lista *ptrL){
+    if(ptrL->n+1==TAML){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int lista_inserirFim(Lista *ptrL, Contato contato){
+    if(lista_cheia(ptrL)){
+        printf("Lista cheia!");
+        return 1;
+    }
+    ptrL->n++;
+    ptrL ->vetor[ptrL->n]=contato;
+    return 0;
+}
+
