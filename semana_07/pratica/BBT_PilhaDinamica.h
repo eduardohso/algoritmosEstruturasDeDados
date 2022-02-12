c3c3282399a2964d751c938ed1f4228f42e9f73d
Eduardo Oliveira <eduardohsoliveira1@gmail.com>
2022-02-11 22:33:30 -0300

    pratica finalizada

--- semana_07/pratica/BBT_PilhaDinamica.h
+++ semana_07/pratica/BBT_PilhaDinamica.h
@@ -7,11 +7,64 @@ typedef struct sLivro
     int numP;
 }Livro;
 
-
 typedef struct sNO
 {
     Livro info;
     struct sNO *prox;
 }NO;
 
+void inicializar(NO** topo){
+    *topo=NULL;
+}
 
+NO* alocarNO(){
+    return (NO*) malloc (sizeof(NO));
+}
+void desalocarNO (NO *lista){
+    free(lista);
+}
+
+int vazia(NO* topo){
+    if(topo==NULL)
+        return 1;
+    else 
+        return 0;
+}
+
+void empilhar(NO** topo, Livro elem){
+    NO* novo = alocarNO();
+    if(novo!=NULL){
+        novo->info=elem;
+        novo->prox=*topo;
+        *topo=novo;
+        printf("Elemento empilhado com sucesso.\n");
+    }else{
+        printf("Erro na alocacao.\n");
+    }
+}
+
+Livro desempilhar(NO** topo){
+    if(!vazia(*topo)){
+        Livro L;
+        NO* aux;
+        aux = *topo;
+        *topo = aux->prox;
+        L = aux->info;
+        desalocarNO(aux);
+        return L;
+    }else{
+        printf("A pilha esta vazia.\n");
+    }
+}
+
+void impressaoNaoClassica(NO* topo){
+    if(!vazia(topo)){
+        NO* aux=topo;
+        while(aux!=NULL){
+            printf("\nLivro \'%s\' possui %d paginas.\n",aux->info.titulo,aux->info.numP);
+            aux=aux->prox;
+        }
+    }else{
+        printf("A pilha esta vazia.\n");
+    }
+}
--- semana_07/pratica/main_PilhaDinamica.c
+++ semana_07/pratica/main_PilhaDinamica.c
@@ -4,5 +4,46 @@
 #include "BBT_PilhaDinamica.h"
 
 int main(){
-    
+    NO* Pilha;
+    int op;
+    Livro book;
+
+    inicializar(&Pilha);
+    do{
+        printf("\n\nDigite uma opcao:\n");
+        printf("1 - Inserir um livro no topo da pilha.\n");
+        printf("2 - Remover um livro do topo da pilha.\n");
+        printf("3 - Exibir elementos da pilha.\n");
+        printf("4 - Sair.\n");
+        scanf("%d",&op);
+        fflush(stdin);
+        Livro L;
+        switch (op)
+        {
+        case 1:
+            printf("\nDigite o titulo do livro: ");
+            gets(book.titulo);
+            fflush(stdin);
+            printf("\nDigite o numero de paginas: ");
+            scanf("%d",&book.numP);
+            fflush(stdin);
+            empilhar(&Pilha,book);
+            break;
+        case 2:
+            L = desempilhar(&Pilha);
+            printf("\nLivro \'%s\', %d paginas foi desempilhado com sucesso.",L.titulo,L.numP);
+            break;
+        case 3:
+            impressaoNaoClassica(Pilha);
+            break;
+        case 4:
+            printf("\nSaindo.");
+            break;
+        default:
+        printf("Opcao invalida!\n");
+            break;
+        }
+    }while(op!=4);
+
+    return 0;
 }
