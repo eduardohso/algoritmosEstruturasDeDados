0259d60bca3884b02f51f769717f458e46290575
Eduardo Oliveira <eduardohsoliveira1@gmail.com>
2022-02-18 21:57:58 -0300

    pratica finalizada

--- semana_08/pratica/BBT_fila.h
+++ semana_08/pratica/BBT_fila.h
@@ -20,5 +20,64 @@ typedef struct sFila
 }Fila;
 
 void inic(Fila *ptr){
-    
+    ptr->inicio=NULL;
+    ptr->fim=NULL;
+}
+
+int vazia(Fila *ptr){
+    if(ptr -> inicio == NULL)
+        return 1;
+    return 0;
+}
+
+NO* alocarNO(){
+    return (NO*) malloc (sizeof(NO));
+}
+
+void enfileirar(Fila *ptr, Pessoa elem){
+    NO* novo = alocarNO();
+    if(novo!=NULL){
+        novo->info=elem;
+        novo->prox=NULL;
+        if(vazia(ptr)){
+            ptr->inicio=novo;
+            ptr->fim=novo;
+        }else{
+            (ptr->fim)->prox=novo;
+            ptr->fim=novo;
+        }
+    }else{
+        printf("Erro na alocacao!\n");
+    }
+}
+
+void impressaoNaoClassica(Fila *ptr){
+    if(!vazia(ptr)){
+        NO* aux=ptr->inicio;
+        while (aux!=NULL)
+        {
+            printf("Pessoa %s vai ",aux->info.nome);    
+            switch (aux->info.opBancaria)
+            {
+            case 'S':
+            case 's':
+                printf("SACAR uma quantia.\n");
+                break;
+            case 'D':
+            case 'd':
+                printf("Depositar uma quantia.\n");
+                break;
+            case 'P':
+            case 'p':
+                printf("EFETUAR UM PAGAMENTO\n");
+                break;
+            default:
+                break;
+            }
+            aux=aux->prox;
+        }
+            printf("\n");
+    }else{
+        printf("\nFila vazia!\n");
+    }
 }
--- semana_08/pratica/main.c
+++ semana_08/pratica/main.c
@@ -8,5 +8,45 @@ int main(){
     Pessoa pes;
     Fila *fila;
 
+    fila = (Fila*) malloc (sizeof(Fila));
+
+    if(fila!=NULL){
+
+        inic(fila);
+
+        do{
+            printf("\nSelecione uma opcao:\n");
+            printf("1 - Inserir um elemento na fila.\n");
+            printf("2 - Exibir os elementos da fila (nao classica).\n");
+            printf("3 - Sair.\n");
+            printf("\n--> ");
+            scanf("%d",&op);
+            fflush(stdin);
+            switch (op)
+            {
+            case 1:
+                printf("\nDigite o nome do proprietario da conta: ");
+                fflush(stdin);
+                gets(pes.nome);
+                fflush(stdin);
+
+                printf("\nEspecifique a atividade bancaria: \n");
+                printf("--> S para sacar \n");
+                printf("--> D para depositar\n");
+                printf("--> P para efetuar pagamento\n");
+                printf("\n--> ");
+                scanf("%c", &pes.opBancaria);
+
+                enfileirar(fila,pes);
+                break;
+            case 2:
+                impressaoNaoClassica(fila);
+                break;
+            default:
+                printf("Saindo.\n");
+                break;
+            }
+        }while(op!=3);
+    }
     return 0;
 }
